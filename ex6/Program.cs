09353a2e07a71d9ffde5e5b7ead081848c0a7584
Ivan Gurov <guuuroov@gmail.com>
2023-03-06 17:24:09 +0300

    ex6

---
+++ ex6/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Your name: ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "ivan")
+{
+  Console.WriteLine("Nice to see you again, Ivan!");
+}
+else
+{
+  Console.Write("Hello, ");
+  Console.WriteLine(username);
+}
